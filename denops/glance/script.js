7831bd8d5f2f21dfcfbfa803c3a8cb68db1a9942
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-12-05 11:35:04 +0900

    Update script.js

--- denops/glance/script.js
+++ denops/glance/script.js
@@ -1,21 +1,18 @@
 import morphdom from "https://unpkg.com/morphdom/dist/morphdom-esm.js";
 const ws = new WebSocket(`ws://${location.host}/ws`);
-let html = "";
 
 async function update(payload) {
-  //console.log(payload);
-  const doc = document.getElementById("viewer").contentWindow.document;
   // update html
+  const doc = document.getElementById("viewer").contentWindow.document;
   const root = doc.getElementById("root");
-  if (root) {
-    morphdom(root, `<div id="root">${payload.document}</div>`);
-  }
+  root && morphdom(root, `<div id="root">${payload.document}</div>`);
   // update curosr position
   let line = payload.line;
-  let element = doc.querySelector(`[data-source-line="${line}"],[id="data-source-line-${line}"]`);
+  const query = `[data-source-line="${line}"],[id="data-source-line-${line}"]`;
+  let element = doc.querySelector(query);
   while (line > 1 && !element) {
     line--;
-    element = doc.querySelector(`[data-source-line="${line}"],[id="data-source-line-${line}"]`);
+    element = doc.querySelector(query);
   }
   element?.scrollIntoView();
 }
